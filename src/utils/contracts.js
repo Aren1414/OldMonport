f283cc1b7c472718ca9d3544c3834014f0aa2b2b
Aren <71951258+Aren1414@users.noreply.github.com>
2025-05-19 21:48:00 +0330

    Update contracts.js

--- src/utils/contracts.js
+++ src/utils/contracts.js
@@ -1,12 +1,14 @@
-export const WELCOME_NFT_ADDRESS = "0x40649af9dEE8bDB94Dc21BA2175AE8f5181f14AE";
-export const CROC_SWAP_ADDRESS = "0x88B96aF200c8a9c35442C8AC6cd3D22695AaE4F0";
-export const MONPORT_FACTORY_ADDRESS = "0x3FFA88641129f4Ac39287Bd2768A165Ac6055e31";
-
 // ABIs
 import WelcomeNFT_ABI from "../abis/WelcomeNFT.json";
 import CrocSwap_ABI from "../abis/CrocSwapDex.json";
 import MonPortFactory_ABI from "../abis/MonPortFactory.json";
 
+// Contract addresses
+export const WELCOME_NFT_ADDRESS = "0x40649af9dEE8bDB94Dc21BA2175AE8f5181f14AE";
+export const CROC_SWAP_ADDRESS = "0x88B96aF200c8a9c35442C8AC6cd3D22695AaE4F0";
+export const MONPORT_FACTORY_ADDRESS = "0x3FFA88641129f4Ac39287Bd2768A165Ac6055e31";
+
+// ABIs exported
 export const WELCOME_NFT_ABI = WelcomeNFT_ABI;
 export const CROC_SWAP_ABI = CrocSwap_ABI;
 export const MONPORT_FACTORY_ABI = MonPortFactory_ABI;
