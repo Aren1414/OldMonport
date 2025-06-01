834b86a9a4a3d2ce3de8ce8cf05b97325defa91b
Aren1414 <aren.silver12@gmail.com>
2025-06-01 17:01:52 +0330

    remove: delete index.js from root

--- index.js
+++
@@ -1,7 +0,0 @@
-const { PoolFetcher } = require('@kuru-labs/kuru-sdk');
-
-const baseUrl = 'https://api.kuru.io';
-
-const fetcher = new PoolFetcher(baseUrl);
-
-console.log('Available methods on PoolFetcher:', Object.getOwnPropertyNames(Object.getPrototypeOf(fetcher)));
